8ce46f8126277d6409a41debd1343a0504e1c998
estefpreci327-ship-it <estefpreci3.27@gmail.com>
2026-07-09 15:21:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+let klowSlide = 1;
+const klowTotal = 14;
 
+function openKlowGallery(){
+  document.getElementById("klowModal").style.display = "block";
+  showKlowSlide();
+}
+
+function closeKlowGallery(){
+  document.getElementById("klowModal").style.display = "none";
+}
+
+function changeKlowSlide(direction){
+  klowSlide += direction;
+
+  if(klowSlide < 1){
+    klowSlide = klowTotal;
+  }
+
+  if(klowSlide > klowTotal){
+    klowSlide = 1;
+  }
+
+  showKlowSlide();
+}
+
+function showKlowSlide(){
+  document.getElementById("klowImage").src = "images/klow-guide-" + klowSlide + ".jpg";
+  document.getElementById("klowCounter").textContent = klowSlide + " / " + klowTotal;
+}

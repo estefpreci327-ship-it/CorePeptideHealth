bf893d9dfcaf055b30b9e5a2dff2b9630be8da23
estefpreci327-ship-it <estefpreci3.27@gmail.com>
2026-07-08 22:24:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-function setLanguage(language) {
-  document.querySelectorAll("[data-en]").forEach(element => {
-    element.textContent = element.getAttribute(`data-${language}`);
-  });
 
-  localStorage.setItem("language", language);
-}
-
-window.onload = function () {
-  const savedLanguage = localStorage.getItem("language") || "en";
-  setLanguage(savedLanguage);
-};
